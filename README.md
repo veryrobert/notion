8e001185c261bf436dda38769d277c8760d33c40
veryrobert <hello@robertfarrelly.com>
2013-06-07 16:54:50 +0100

    Add bxslider cleared out old kit files and moved all elements into a build folder for easy upload

--- README.md
+++
@@ -1,5 +0,0 @@
-# Design by Notion Website — branch nv1
-
-This is version one of the new Notion website. Previous versions have been used to understand sass and gridset and how they work together.
-
-Here I will begin to develop the Notion website using some new techniques I have never previously tried, including: Gridset, Sass, Codekit and Compass.
--- config.rb
+++ config.rb
@@ -2,10 +2,10 @@
 
 # Set this to the root of your project when deployed:
 http_path = "/"
-css_dir = "css"
+css_dir = "build/css"
 sass_dir = "sass"
-images_dir = "img/"
-javascripts_dir = "js/"
+images_dir = "build/img/"
+javascripts_dir = "build/js/"
 
 # You can select your preferred output style here (can be overridden via the command line):
 # output_style = :expanded or :nested or :compact or :compressed
